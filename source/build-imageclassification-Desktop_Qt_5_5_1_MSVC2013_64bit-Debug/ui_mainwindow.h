c1651f774cac3defe89265a74c306ce80bceeca3
zhounanzhao <414293496@qq.com>
2015-12-08 17:41:35 +0800

    Signed-off-by: zhounanzhao <414293496@qq.com>

---
+++ source/build-imageclassification-Desktop_Qt_5_5_1_MSVC2013_64bit-Debug/ui_mainwindow.h
@@ -0,0 +1,92 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.5.1
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QAction>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralWidget;
+    QMenuBar *menuBar;
+    QMenu *menuImageclassification;
+    QMenu *menuTrain;
+    QMenu *menuClassify;
+    QMenu *menuHelp;
+    QToolBar *mainToolBar;
+    QStatusBar *statusBar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
+        MainWindow->resize(871, 583);
+        centralWidget = new QWidget(MainWindow);
+        centralWidget->setObjectName(QStringLiteral("centralWidget"));
+        MainWindow->setCentralWidget(centralWidget);
+        menuBar = new QMenuBar(MainWindow);
+        menuBar->setObjectName(QStringLiteral("menuBar"));
+        menuBar->setGeometry(QRect(0, 0, 871, 23));
+        menuImageclassification = new QMenu(menuBar);
+        menuImageclassification->setObjectName(QStringLiteral("menuImageclassification"));
+        menuTrain = new QMenu(menuBar);
+        menuTrain->setObjectName(QStringLiteral("menuTrain"));
+        menuClassify = new QMenu(menuBar);
+        menuClassify->setObjectName(QStringLiteral("menuClassify"));
+        menuHelp = new QMenu(menuBar);
+        menuHelp->setObjectName(QStringLiteral("menuHelp"));
+        MainWindow->setMenuBar(menuBar);
+        mainToolBar = new QToolBar(MainWindow);
+        mainToolBar->setObjectName(QStringLiteral("mainToolBar"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, mainToolBar);
+        statusBar = new QStatusBar(MainWindow);
+        statusBar->setObjectName(QStringLiteral("statusBar"));
+        MainWindow->setStatusBar(statusBar);
+
+        menuBar->addAction(menuImageclassification->menuAction());
+        menuBar->addAction(menuTrain->menuAction());
+        menuBar->addAction(menuClassify->menuAction());
+        menuBar->addAction(menuHelp->menuAction());
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "imageclassification", 0));
+        menuImageclassification->setTitle(QApplication::translate("MainWindow", "manageImageLib", 0));
+        menuTrain->setTitle(QApplication::translate("MainWindow", "train", 0));
+        menuClassify->setTitle(QApplication::translate("MainWindow", "classify", 0));
+        menuHelp->setTitle(QApplication::translate("MainWindow", "help", 0));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
---
+++ source/imageclassification/imageclassification.pro
@@ -0,0 +1,23 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-12-08T16:05:19
+#
+#-------------------------------------------------
+
+QT       += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+TARGET = imageclassification
+TEMPLATE = app
+
+
+SOURCES += main.cpp\
+        mainwindow.cpp
+
+HEADERS  += mainwindow.h
+
+FORMS    += mainwindow.ui
+
+RESOURCES += \
+    resoures.qrc
